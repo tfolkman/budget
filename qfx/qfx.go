8fdf62438f8b7fb98029b591cf535128d8922bdf
Tyler Folkman <tylerfolkman@gmail.com>
2016-10-19 22:06:35 -0600

    have a start to perhaps a qfx fix

--- qfx/qfx.go
+++ qfx/qfx.go
@@ -2,71 +2,56 @@ package qfx
 
 import (
 	"log"
-	"bufio"
+	"encoding/xml"
 	"os"
-	"strings"
-	"time"
-	"strconv"
-	"github.com/tfolkman/budget/models"
-	"github.com/astaxie/beego/orm"
 )
+
+type Transaction struct {
+	trnType string   `xml:"TRNTYPE"`
+	datePosted string   `xml:"DTPOSTED"`
+	trnAmt float64`xml:"TRNAMT"`
+	name string`xml:"NAME"`
+}
+
+type Result struct {
+	XMLName xml.Name `xml:"OFX"`
+	Transactions []Transaction `xml:"STMTTRN"`
+}
+
 
 func ReadQfx(fileName string) {
 	log.Println("Read QFX...")
-	o := orm.NewOrm()
-	file, err := os.Open(fileName)
+	//o := orm.NewOrm()
+	xmlFile, err := os.Open(fileName)
         if err != nil {
             log.Fatal(err)
         }
-        defer file.Close()
-
-	var date time.Time
-	var outflow float64
-	var inflow float64
-	var credit bool
-	var payee string
-
-        scanner := bufio.NewScanner(file)
-        for scanner.Scan() {
-                text := scanner.Text()
-		text = strings.TrimSpace(text)
-		if strings.Contains(text, "<TRNTYPE>"){
-			if strings.Contains(text, "CREDIT"){
-				credit = true
-			} else {
-				credit = false
+	v := Result{}
+	decoder := xml.NewDecoder(xmlFile)
+	var inElement string
+	//var trnType string
+	//var datePosted string
+	//var trnAmt float64
+	//var name string
+	for {
+		// Read tokens from the XML document in a stream.
+		t, _ := decoder.RawToken()
+		log.Println(t)
+		if t == nil {
+			break
+		}
+		switch se := t.(type) {
+		case xml.EndElement:
+			// If we just read a StartElement token
+			inElement = se.Name.Local
+			// ...and its name is "page"
+			if inElement == "STMTTRN" {
+				log.Println(inElement)
 			}
+		default:
 		}
-		if strings.Contains(text, "<DTPOSTED>"){
-			date, _ = time.Parse("20060102", text[10:18])
-		}
-		if strings.Contains(text, "<TRNAMT>"){
-			if credit {
-				num, _ := strconv.ParseFloat(text[8:len(text)], 64)
-				inflow = num
-				outflow = 0.0
-			} else {
-				num, _ := strconv.ParseFloat(text[9:len(text)], 64)
-				outflow = num
-				inflow = 0.0
-			}
-		}
-		if strings.Contains(text, "<NAME>"){
-			payee = text[6:len(text)]
-		}
-		if strings.Contains(text, "</STMTTRN>"){
-			importData := new(models.Transactions)
-			importData.Date = date
-			importData.Inflow = inflow
-			importData.Outflow = outflow
-			importData.Payee = payee
-			importData.Import = true
-			log.Println(payee)
-			log.Println(o.Insert(importData))
-		}
-        }
+	}
 
-        if err := scanner.Err(); err != nil {
-            log.Fatal(err)
-        }
+	log.Println(v.Transactions)
+	//log.Println(o.Insert(importData))
 }
