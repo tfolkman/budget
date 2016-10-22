dc64ceb66e6103232c5eb4f934464e607131a3cc
Tyler Folkman <tylerfolkman@gmail.com>
2016-10-22 13:46:19 -0600

    figured out how to go from token to char data

--- qfx/qfx.go
+++ qfx/qfx.go
@@ -29,6 +29,7 @@ func ReadQfx(fileName string) {
 	v := Result{}
 	decoder := xml.NewDecoder(xmlFile)
 	var inElement string
+	var text string
 	//var trnType string
 	//var datePosted string
 	//var trnAmt float64
@@ -36,17 +37,59 @@ func ReadQfx(fileName string) {
 	for {
 		// Read tokens from the XML document in a stream.
 		t, _ := decoder.RawToken()
-		log.Println(t)
 		if t == nil {
 			break
 		}
 		switch se := t.(type) {
+		case xml.CharData:
+			if inElement == "TRNTYPE"{
+				log.Println(string(t.(xml.CharData)))
+			}
+		case xml.StartElement:
+			inElement = se.Name.Local
+			if inElement == "TRNTYPE" {
+				log.Println(inElement)
+				//decoder.DecodeElement(&text, &se)
+				log.Println(text)
+			}
+			//if strings.Contains(text, "<TRNTYPE>"){
+			//	if strings.Contains(text, "CREDIT"){
+			//		credit = true
+			//	} else {
+			//		credit = false
+			//	}
+			//}
+			//if strings.Contains(text, "<DTPOSTED>"){
+			//	date, _ = time.Parse("20060102", text[10:18])
+			//}
+			//if strings.Contains(text, "<TRNAMT>"){
+			//	if credit {
+			//		num, _ := strconv.ParseFloat(text[8:len(text)], 64)
+			//		inflow = num
+			//		outflow = 0.0
+			//	} else {
+			//		num, _ := strconv.ParseFloat(text[9:len(text)], 64)
+			//		outflow = num
+			//		inflow = 0.0
+			//	}
+			//}
+			//if strings.Contains(text, "<NAME>"){
+			//	payee = text[6:len(text)]
+			//}
 		case xml.EndElement:
 			// If we just read a StartElement token
 			inElement = se.Name.Local
 			// ...and its name is "page"
 			if inElement == "STMTTRN" {
 				log.Println(inElement)
+				//importData := new(models.Transactions)
+				//importData.Date = date
+				//importData.Inflow = inflow
+				//importData.Outflow = outflow
+				//importData.Payee = payee
+				//importData.Import = true
+				//log.Println(payee)
+				//log.Println(o.Insert(importData))
 			}
 		default:
 		}
