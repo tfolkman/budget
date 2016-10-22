f63325287a4b147121aa65e3600c76e4633438d0
Tyler Folkman <tylerfolkman@gmail.com>
2016-10-22 13:56:57 -0600

    seem to have qfx working with xml parser

--- qfx/qfx.go
+++ qfx/qfx.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"encoding/xml"
 	"os"
+	"strings"
+	"time"
+	"strconv"
 )
 
 type Transaction struct {
@@ -13,12 +16,6 @@ type Transaction struct {
 	name string`xml:"NAME"`
 }
 
-type Result struct {
-	XMLName xml.Name `xml:"OFX"`
-	Transactions []Transaction `xml:"STMTTRN"`
-}
-
-
 func ReadQfx(fileName string) {
 	log.Println("Read QFX...")
 	//o := orm.NewOrm()
@@ -26,14 +23,14 @@ func ReadQfx(fileName string) {
         if err != nil {
             log.Fatal(err)
         }
-	v := Result{}
 	decoder := xml.NewDecoder(xmlFile)
 	var inElement string
 	var text string
-	//var trnType string
-	//var datePosted string
-	//var trnAmt float64
-	//var name string
+	var credit bool
+	var date time.Time
+	var inflow float64
+	var outflow float64
+	var payee string
 	for {
 		// Read tokens from the XML document in a stream.
 		t, _ := decoder.RawToken()
@@ -42,45 +39,43 @@ func ReadQfx(fileName string) {
 		}
 		switch se := t.(type) {
 		case xml.CharData:
+			text = string(t.(xml.CharData))
 			if inElement == "TRNTYPE"{
-				log.Println(string(t.(xml.CharData)))
+				if strings.Contains(text, "CREDIT"){
+					credit = true
+				} else {
+					credit = false
+				}
+			}
+			if inElement == "DTPOSTED"{
+				date, _ = time.Parse("20060102", text[:8])
+			}
+			if inElement == "TRNAMT"{
+				if credit {
+					num, _ := strconv.ParseFloat(text, 64)
+					inflow = num
+					outflow = 0.0
+				} else {
+					num, _ := strconv.ParseFloat(text[1:len(text)], 64)
+					outflow = num
+					inflow = 0.0
+				}
+			}
+			if inElement == "NAME"{
+				payee = text
 			}
 		case xml.StartElement:
 			inElement = se.Name.Local
-			if inElement == "TRNTYPE" {
-				log.Println(inElement)
-				//decoder.DecodeElement(&text, &se)
-				log.Println(text)
-			}
-			//if strings.Contains(text, "<TRNTYPE>"){
-			//	if strings.Contains(text, "CREDIT"){
-			//		credit = true
-			//	} else {
-			//		credit = false
-			//	}
-			//}
-			//if strings.Contains(text, "<DTPOSTED>"){
-			//	date, _ = time.Parse("20060102", text[10:18])
-			//}
-			//if strings.Contains(text, "<TRNAMT>"){
-			//	if credit {
-			//		num, _ := strconv.ParseFloat(text[8:len(text)], 64)
-			//		inflow = num
-			//		outflow = 0.0
-			//	} else {
-			//		num, _ := strconv.ParseFloat(text[9:len(text)], 64)
-			//		outflow = num
-			//		inflow = 0.0
-			//	}
-			//}
-			//if strings.Contains(text, "<NAME>"){
-			//	payee = text[6:len(text)]
-			//}
 		case xml.EndElement:
 			// If we just read a StartElement token
 			inElement = se.Name.Local
 			// ...and its name is "page"
 			if inElement == "STMTTRN" {
+				log.Println(date)
+				log.Println(inflow)
+				log.Println(outflow)
+				log.Println(payee)
+				log.Println(credit)
 				log.Println(inElement)
 				//importData := new(models.Transactions)
 				//importData.Date = date
@@ -95,6 +90,5 @@ func ReadQfx(fileName string) {
 		}
 	}
 
-	log.Println(v.Transactions)
 	//log.Println(o.Insert(importData))
 }
