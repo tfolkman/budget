74b92466aed0a258a17d3869f4430c4db014b2ad
Tyler Folkman <tylerfolkman@gmail.com>
2016-11-01 21:27:36 -0600

    reverted dropbox sync stuff for now

--- README.md
+++ README.md
@@ -2,15 +2,10 @@
 
 A simple budget application
 
-## Sync Data
-
-The app currently supports Dropbox syncing.
-To do so, create an app-specific API key here: https://www.dropbox.com/developers/apps/create. Name the app "simple-budget-data".
-
 ## Road Map
 
-1. Fix income budget
-2. Dockerize
+1. Dockerize
+2. Figure out way to sync to Dropbox
 3. Add total monthly budget and spend on main page
 4. QIF import for paypal
 5. Dashboards
--- main.go
+++ main.go
@@ -8,48 +8,14 @@ import (
 	"github.com/tfolkman/budget/models"
 	"log"
 	"time"
-	"os"
-	"github.com/astaxie/beego/toolbox"
-	"encoding/json"
-	"io/ioutil"
-	"github.com/tj/go-dropbox"
 )
 
-type DropboxAuth struct {
-	Key string `json:"key"`
-	Secret string `json:"secret"`
-	Token string `json:"token"`
-}
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DRSqlite)
 	orm.RegisterDataBase("default", "sqlite3", "./data/budget.db")
 	orm.RegisterModel(new(models.Budget))
 	orm.RegisterModel(new(models.Transactions))
-
-	if _, err := os.Stat("./data/dropbox.json"); err == nil {
-		log.Println("setting up task...")
-		tk1 := toolbox.NewTask("tk1", "0/15 * * * * *", func() error {
-			var dropboxAuth DropboxAuth
-			file, _ := ioutil.ReadFile("./data/dropbox.json")
-			dbFile , _ := os.Open("./data/budget.db")
-    			json.Unmarshal(file, &dropboxAuth)
-			log.Println(dropboxAuth.Token)
-			d := dropbox.New(dropbox.NewConfig(dropboxAuth.Token))
-			_, err := d.Files.Upload(&dropbox.UploadInput{
-				Path:   "budget.db",
-				Reader: dbFile,
-				Mute:   false,
-			})
-			if err != nil {
-				log.Println(err)
-			}
-			log.Println("dropbox...")
-			return nil
-		})
-		toolbox.AddTask("tk1", tk1)
-		toolbox.StartTask()
-	}
 }
 
 func main() {
